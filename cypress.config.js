c37f6218349dd2f6e562f10bc24d27ee17bc1ecc
Namankumar Kalola <namankalola@yahoo.com>
2025-04-01 15:19:40 +0530

    Sample script

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ern11c',
   e2e: {
+    specPattern : "cypress/testcases/sample/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
